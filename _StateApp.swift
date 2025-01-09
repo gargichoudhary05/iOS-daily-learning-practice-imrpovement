d8f810c98f9e928ffd551c212d41a92122898e0b
gargichoudhary05 <66163751+gargichoudhary05@users.noreply.github.com>
2025-01-09 18:47:22 +0530

    Add files via upload

---
+++ _StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  _StateApp.swift
+//  @State
+//
+//  Created by Ananya Choudhary on 09/01/25.
+//
+
+import SwiftUI
+
+@main
+struct _StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
